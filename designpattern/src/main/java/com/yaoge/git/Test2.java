43599154b85c65eb389dc5c74a5ebfa4fc0b5040
faker <faker@163.com>
2022-08-17 11:14:10 +0800

    master 第一次提交

---
+++ designpattern/src/main/java/com/yaoge/git/Test2.java
@@ -0,0 +1,11 @@
+package com.yaoge.git;
+
+/**
+ * create by yaoge
+ * 2022/8/17 11:12
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("master 第一次提交");
+    }
+}
